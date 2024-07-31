010a697ffbbf77100cd90eb93359e6779a36e04c
Diego Morales <77738746+Diego2250@users.noreply.github.com>
2024-07-30 19:15:50 -0600

    conexión entre emisor y receptor

--- Hamming/Hamming.py
+++ Hamming/Hamming.py
@@ -1,19 +1,16 @@
 import math
+import socket
+import random
 
 def calcular_bits_paridad(mensaje, r):
-    # Inicializamos la lista para guardar las posiciones de los bits de paridad
     bp_pos = []
-
-    # Insertamos los bits de paridad en el mensaje y guardamos sus posiciones
     for i in range(r):
         posicion = int(math.pow(2, i))
         mensaje.insert(posicion - 1, '0')
         bp_pos.append(posicion)
-
     return bp_pos
 
 def actualizar_bits_paridad(mensaje, bp_pos):
-    # Calculamos y actualizamos los bits de paridad en el mensaje
     for posicion in bp_pos:
         contador = 0
         for j in range(1, len(mensaje) + 1):
@@ -22,25 +19,45 @@ def actualizar_bits_paridad(mensaje, bp_pos):
         mensaje[posicion - 1] = '0' if contador % 2 == 0 else '1'
 
 def hamming(mensaje):
-    mensaje = mensaje[::-1]  # Invertimos el mensaje para facilitar la inserción de bits de paridad
+    mensaje = mensaje[::-1]
     m = len(mensaje)
     r = 0
-
-    # Calculamos el número de bits de paridad necesarios
     while math.pow(2, r) < m + r + 1:
         r += 1
+    bp_pos = calcular_bits_paridad(mensaje, r)
+    actualizar_bits_paridad(mensaje, bp_pos)
+    return mensaje[::-1]
 
-    bp_pos = calcular_bits_paridad(mensaje, r)  # Calculamos las posiciones de los bits de paridad
-    actualizar_bits_paridad(mensaje, bp_pos)    # Actualizamos los bits de paridad en el mensaje
+def aplicar_ruido(mensaje, tasa_error):
+    for i in range(len(mensaje)):
+        if random.random() < tasa_error / 100:  # Convertimos la tasa de error a porcentaje
+            mensaje[i] = '0' if mensaje[i] == '1' else '1'
+            print(f"Bit {i + 1} alterado")
+            return mensaje
+    return mensaje
 
-    return mensaje[::-1]  # Devolvemos el mensaje a su orden original
+def mensaje_a_binario(mensaje):
+    return ''.join(format(ord(c), '08b') for c in mensaje)
 
 def main():
-    mensaje = input("Ingrese el mensaje a codificar: ")
-    mensaje = list(mensaje)
+    mensajes = ["Hola", "Mundo", "Como", "Estas", "Hoy", "Es", "Un", "Buen", "Dia", "Para", "Aprender", "Sobre", "Redes", "De", "Computadoras", "Y", "Comunicaciones"]
+    probabilidades = [5, 10, 15, 20, 25, 30, 25, 20, 15, 10, 5, 10, 15, 20, 25, 30, 80]
 
-    mensaje_codificado = hamming(mensaje)
-    print("Mensaje codificado: ", ''.join(mensaje_codificado))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(('localhost', 65432))
+        for mensaje, tasa_error in zip(mensajes, probabilidades):
+            print(f"Mensaje seleccionado: {mensaje} con tasa de error: {tasa_error}%")
+            mensaje_binario = list(mensaje_a_binario(mensaje))
+            mensaje_codificado = hamming(mensaje_binario)
+            print(f"Mensaje codificado: {''.join(mensaje_codificado)}")
+            mensaje_con_ruido = aplicar_ruido(mensaje_codificado.copy(), tasa_error)
+            print(f"Mensaje con ruido: {''.join(mensaje_con_ruido)}")
+            mensaje_final = ''.join(mensaje_con_ruido)
+            s.sendall(mensaje_final.encode())
+            print(f"Mensaje enviado: {mensaje_final}")
+            
+            input("Presione Enter para enviar el siguiente mensaje...")
 
 if __name__ == "__main__":
     main()
+
---
+++ Hamming/Hamminng/Hamminng/Hamminng/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class HammingDecoder
+{
+    public static string Decode(string message)
+    {
+        int[] msg = Array.ConvertAll(message.ToCharArray(), c => c - '0');
+        Array.Reverse(msg);
+
+        int r = 0;
+        for (int i = 0; i < msg.Length; i++)
+        {
+            if ((i & (i + 1)) == 0)
+            {
+                r++;
+            }
+        }
+
+        int errorPos = 0;
+        for (int i = 0; i < r; i++)
+        {
+            int x = 1 << i;
+            int parity = 0;
+            for (int j = x - 1; j < msg.Length; j += 2 * x)
+            {
+                for (int k = j; k < j + x && k < msg.Length; k++)
+                {
+                    parity ^= msg[k];
+                }
+            }
+            if (parity != 0)
+            {
+                errorPos += x;
+            }
+        }
+
+        if (errorPos != 0)
+        {
+            if (errorPos - 1 < msg.Length)
+            {
+                Console.WriteLine("Error en la posición: " + errorPos);
+                msg[errorPos - 1] ^= 1;
+            }
+        }
+        else
+        {
+            Console.WriteLine("No hay errores");
+        }
+
+        List<int> data = new List<int>();
+        for (int i = 0; i < msg.Length; i++)
+        {
+            if ((i & (i + 1)) != 0)
+            {
+                data.Add(msg[i]);
+            }
+        }
+
+        data.Reverse();
+        return string.Join("", data);
+    }
+
+    public static string BinarioATexto(string binario)
+    {
+        var stringBuilder = new StringBuilder();
+        for (int i = 0; i < binario.Length; i += 8)
+        {
+            string byteString = binario.Substring(i, 8);
+            byte byteValue = Convert.ToByte(byteString, 2);
+            stringBuilder.Append((char)byteValue);
+        }
+        return stringBuilder.ToString();
+    }
+
+    public static void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 65432);
+        server.Start();
+        Console.WriteLine("Esperando conexiones...");
+        while (true)
+        {
+            using (TcpClient client = server.AcceptTcpClient())
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Mensaje recibido: " + receivedMessage);
+
+                    string decodedBits = Decode(receivedMessage);
+                    string decodedMessage = BinarioATexto(decodedBits);
+                    Console.WriteLine("Mensaje decodificado: " + decodedMessage);
+                }
+            }
+        }
+    }
+}
